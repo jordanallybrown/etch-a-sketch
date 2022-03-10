a4c8bd5b5d3f63b8c347af1054d3fe0963cbfa79
Jordan <jordanallybrown@gmail.com>
2022-03-09 18:18:28 -0600

    modify size via popup query to user to clear grid

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
-let gridSize = 32;
+const COLOR = 'purple';
+const DEFAULT_GRID_SIZE = 16;
 
-const container = document.querySelector('#container');
+const gridContainer = document.querySelector('#container');
+const clearButton = document.querySelector('#clear');
 
-// Generate the divs
-let fragment = document.createDocumentFragment();
-let div;
+function setupGrid(gridSize = DEFAULT_GRID_SIZE){
+    let fragment = document.createDocumentFragment();
+    let div;
 
-//setup the grid column numbers
-container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
-for(let i = 0; i < gridSize; i++) {
-    for(let j = 0; j < gridSize; j++) {
-        div = document.createElement('div');
-        div.addEventListener('mouseenter', (e) => {
-            e.target.style.backgroundColor = "purple";
-        })
-        fragment.appendChild(div);
+    for(let i = 0; i < gridSize; i++) {
+        for(let j = 0; j < gridSize; j++) {
+            div = document.createElement('div');
+            div.addEventListener('mouseenter', (e) => {
+                e.target.style.backgroundColor = COLOR;
+            })
+            fragment.appendChild(div);
+        }
     }
+
+    gridContainer.appendChild(fragment);
 }
 
-container.appendChild(fragment);
+function createNewGrid(){
+    while (gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    let size = getGridSize();
+    setupGrid(size);
+}
+
+function getGridSize(){
+    let size;
+    let invalidInput = false;
+    do{
+        size = prompt('Please enter a grid size as a whole number. The maximum size you can enter is 100:');
+        size = parseInt(size)
+        if (typeof(size) === 'number'){
+            invalidInput = (size < 1 || size > 100) ? true : false;
+        }
+    } while(invalidInput)
+    
+    return size;
+    
+}
+
+clearButton.addEventListener('click', () => {
+    createNewGrid();
+});
+
+setupGrid();
+
+
